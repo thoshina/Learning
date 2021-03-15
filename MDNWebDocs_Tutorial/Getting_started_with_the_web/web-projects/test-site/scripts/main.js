ca50c79fbc3123d7ec69ec39c605d8987f3557a8
thoshina <thoshina@it-nasu.com>
2021-03-15 16:52:59 +0900

    ブラウザに `名前` を覚えるよう変更

--- MDNWebDocs_Tutorial/Getting_started_with_the_web/web-projects/test-site/scripts/main.js
+++ MDNWebDocs_Tutorial/Getting_started_with_the_web/web-projects/test-site/scripts/main.js
@@ -1,5 +1,3 @@
-const myHeading = document.querySelector('h1');
-
 let myImage = document.querySelector('img');
 
 myImage.onclick = function() {
@@ -10,3 +8,24 @@ myImage.onclick = function() {
       myImage.setAttribute ('src','images/AirSqrew_4.gif');
     }
 }
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  let myName = prompt('あなたの名前を入力してください。');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Mozilla はすばらしいよ、' + myName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Mozilla はすばらしいよ、' + storedName;
+}
+
