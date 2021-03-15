56e1f9ba96ec26da8c1c0d6335014018107c0a95
thoshina <thoshina@it-nasu.com>
2021-03-15 16:27:11 +0900

    Web サイトの例を膨らませる＞クリックで画像を変更

--- MDNWebDocs_Tutorial/Getting_started_with_the_web/web-projects/test-site/scripts/main.js
+++ MDNWebDocs_Tutorial/Getting_started_with_the_web/web-projects/test-site/scripts/main.js
@@ -1,2 +1,12 @@
 const myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
+
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/AirSqrew_4.gif') {
+      myImage.setAttribute ('src','images/Miyamoto_san_S.png');
+    } else {
+      myImage.setAttribute ('src','images/AirSqrew_4.gif');
+    }
+}
