f99cf19ca5cf2361f691cdff6b93147d2903303a
thoshina <thoshina@it-nasu.com>
2021-03-22 16:25:28 +0900

    空欄または入力キャンセル時の処理を追加

--- MDNWebDocs_Tutorial/Getting_started_with_the_web/web-projects/test-site/scripts/main.js
+++ MDNWebDocs_Tutorial/Getting_started_with_the_web/web-projects/test-site/scripts/main.js
@@ -14,8 +14,11 @@ let myHeading = document.querySelector('h1');
 
 function setUserName() {
   let myName = prompt('あなたの名前を入力してください。');
-  localStorage.setItem('name', myName);
-  myHeading.textContent = 'Mozilla はすばらしいよ、' + myName;
+  if(!myName || myName === null) {
+    setUserName();
+  } else {  localStorage.setItem('name', myName);
+    myHeading.textContent = 'Mozilla はすばらしいよ、' + myName;
+  }
 }
 
 myButton.onclick = function() {
